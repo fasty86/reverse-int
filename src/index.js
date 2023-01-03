d3d5acf09641e68756ed479e2eed9c5ae29fb7aa
fasty86 <oleg.tolm1986@gmail.com>
2023-01-03 20:50:00 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = reverse;
+function reverse(n) {
+    n = Math.abs(n);
+    let str = Array.from(n.toString());
+    return Number(str.reverse().join(""));
 }
+
+// console.log(reverse(1234));
